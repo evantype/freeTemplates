5d10ffd779c710bf209aedb383c317a34f3484f7
evantype <evantype@gmail.com>
2022-07-31 15:34:43 +0300

    консольная команда для запуска парсинга

---
+++ app/Console/Commands/ParseSiteTemplates.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ParseSiteTemplates extends Command
+{
+    protected $signature = 'parse:site_templates';
+
+    protected $description = 'parse site templates';
+
+    public function handle()
+    {
+        return 0;
+    }
+}
