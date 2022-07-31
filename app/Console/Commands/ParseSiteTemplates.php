1160d2f6ec8563e9354ccf88b4ca6ff2e546db74
evantype <evantype@gmail.com>
2022-08-01 01:11:03 +0300

    начало парсинга

--- app/Console/Commands/ParseSiteTemplates.php
+++ app/Console/Commands/ParseSiteTemplates.php
@@ -2,7 +2,20 @@
 
 namespace App\Console\Commands;
 
+use DiDom\Document;
+use Exception;
 use Illuminate\Console\Command;
+
+use Psr\Http\Message\ResponseInterface;
+
+use React\EventLoop\Loop;
+use React\Http\Browser;
+
+use Clue\React\Block;
+
+use function dd;
+
+use const PHP_EOL;
 
 class ParseSiteTemplates extends Command
 {
@@ -12,6 +25,32 @@ class ParseSiteTemplates extends Command
 
     public function handle()
     {
+        echo sprintf("Начинаю парсить...%s", PHP_EOL);
+
+        $client = new Browser();
+
+        $client->get('https://www.themelock.com/pag/1/')->then(function (ResponseInterface $response) use ($client) {
+            $body  = new Document($response->getBody()->__toString());
+            $posts = $body->find('.post');
+            $promises = [];
+            foreach ($posts as $post) {
+                $promises[] = $client->get($post->first('.post__title a')->attr('href'));
+            }
+
+            try {
+                $responses = Block\awaitAll($promises, Loop::get());
+
+                foreach ($responses as $detailResponse) {
+                    $templateBody = new Document($detailResponse->getBody()->__toString());
+                    echo "====================================================" . PHP_EOL;
+                    echo $templateBody->first('.entry-title')->text() . PHP_EOL;
+                    echo "====================================================" . PHP_EOL;
+                }
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
+        });
+
         return 0;
     }
 }
