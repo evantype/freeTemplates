e5abbceb2b0ed3e05510dc3f0f71105a7e70d010
evantype <evantype@gmail.com>
2022-07-31 13:23:51 +0300

    header and footer plus exaple cards

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Free templates') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -18,38 +18,11 @@
 </head>
 <body>
 <div id="app">
-    <header class="p-3 text-bg-dark">
-        <div class="container">
-            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="{{ url('/') }}" class="nav-link px-2 text-secondary">Home</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">About</a></li>
-                </ul>
-
-                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                    <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..."
-                           aria-label="Search">
-                </form>
-
-                <div class="text-end">
-                    @guest
-                        <a href="{{ route('login') }}" class="btn btn-outline-light me-2">Login
-                        </a>
-                        <a href="{{ route('register') }}" class="btn btn-warning">Sign-up</a>
-                    @else
-                        <a href="{{ route('logout') }}" class="btn btn-warning">Logout</a>
-                    @endguest
-                </div>
-            </div>
-        </div>
-    </header>
-
+    @include('partitials.header')
     <main class="py-4">
         @yield('content')
     </main>
 </div>
+@include('partitials.footer')
 </body>
 </html>
