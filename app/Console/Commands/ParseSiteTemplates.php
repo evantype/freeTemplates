24b379d90057a1a822d0b79be34e0d5900a96214
evantype <evantype@gmail.com>
2022-08-11 15:15:27 +0300

    write alpha version parser

--- app/Console/Commands/ParseSiteTemplates.php
+++ app/Console/Commands/ParseSiteTemplates.php
@@ -2,18 +2,19 @@
 
 namespace App\Console\Commands;
 
+use App\Models\SiteTemplate;
 use DiDom\Document;
 use Exception;
 use Illuminate\Console\Command;
-
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use Psr\Http\Message\ResponseInterface;
-
 use React\EventLoop\Loop;
 use React\Http\Browser;
-
 use Clue\React\Block;
 
-use function dd;
+use function array_filter;
+use function explode;
 
 use const PHP_EOL;
 
@@ -30,8 +31,8 @@ class ParseSiteTemplates extends Command
         $client = new Browser();
 
         $client->get('https://www.themelock.com/pag/1/')->then(function (ResponseInterface $response) use ($client) {
-            $body  = new Document($response->getBody()->__toString());
-            $posts = $body->find('.post');
+            $body     = new Document($response->getBody()->__toString());
+            $posts    = $body->find('.post');
             $promises = [];
             foreach ($posts as $post) {
                 $promises[] = $client->get($post->first('.post__title a')->attr('href'));
@@ -40,16 +41,65 @@ class ParseSiteTemplates extends Command
             try {
                 $responses = Block\awaitAll($promises, Loop::get());
 
+                $result = new Collection();
+                $i      = 0;
+
                 foreach ($responses as $detailResponse) {
                     $templateBody = new Document($detailResponse->getBody()->__toString());
-                    echo "====================================================" . PHP_EOL;
-                    echo $templateBody->first('.entry-title')->text() . PHP_EOL;
-                    echo "====================================================" . PHP_EOL;
+
+                    $title          = $templateBody->first('.entry-title')->text();
+                    $download_links = array_filter(
+                        explode(
+                            '<br>',
+                            Str::replace(['<!--QuoteEBegin-->', '<!--QuoteEnd-->'],
+                                '',
+                                $templateBody->first('.single-body .quote')->innerHtml())
+                        ),
+                        fn($item) => !empty($item)
+                    );
+                    $srcLink        = $templateBody->first('.single-body a')->text();
+
+                    Block\await(
+                        $client->get($srcLink)->then(
+                            function (ResponseInterface $response) use ($download_links, $title, $result) {
+                                $sourceBody = new Document($response->getBody()->__toString());
+                                $cover      = $sourceBody->first('.item-preview img')->getAttribute('src');
+                                $demo       = "https://themeforest.net" . $sourceBody->first(
+                                        'a.btn-icon.live-preview'
+                                    )->getAttribute('href');
+
+                                $siteTemplate = new SiteTemplate([
+                                    'title'          => $title,
+                                    'download_links' => $download_links,
+                                    'cover'          => $cover,
+                                    'demo'           => $demo
+                                ]);
+//
+//                                $siteTemplate = new SiteTemplate(
+//                                    compact([
+//                                        'title',
+//                                        'download_links',
+//                                        'cover',
+//                                        'demo'
+//                                    ])
+//                                );
+
+
+                                $siteTemplate->save();
+                            }
+                        )
+                    );
+
+                    $i++;
+                    if ($i > 2) {
+                        break;
+                    }
                 }
             } catch (Exception $e) {
                 echo $e->getMessage();
             }
         });
+
 
         return 0;
     }
