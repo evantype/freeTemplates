63e16ed164231bd00b1450ad5ea9687a1966aea9
evantype <evantype@gmail.com>
2022-08-28 11:35:57 +0300

    поправить верстку списка шаблонов

--- resources/views/site-templates.blade.php
+++ resources/views/site-templates.blade.php
@@ -9,15 +9,14 @@
                 <div class="row">
                     @foreach($siteTemplates as $siteTemplate)
                         <div class="col-sm-6 col-md-4 mb-3">
-                            <a class="d-block" href="{{ route('siteTemplate', ['slug' => $siteTemplate->slug]) }}">
+                            <a class="d-block  text-decoration-none" href="{{ route('siteTemplate', ['slug' => $siteTemplate->slug]) }}">
                                 <img class="img-thumbnail mb-3"
                                      src="{{ asset('storage/site_templates') . "/" . $siteTemplate->cover }}" alt=""
                                      width="480" height="300"
                                      loading="lazy">
-                                <h3 class="h5 mb-1">{{ $siteTemplate->title }}</h3>
+                                <h3 class="h5 mb-1">{{ Str::of($siteTemplate->title)->limit(40) }}</h3>
                             </a>
-                            <p class="text-muted">Display your branding, navigation, search, and more with these header
-                                components</p>
+                            <p class="text-muted">{{ Str::of($siteTemplate->description)->limit(110) }}</p>
                         </div>
                     @endforeach
                 </div>
