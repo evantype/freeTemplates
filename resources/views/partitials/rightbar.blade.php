9746c71c1f01c4f72485e219ddaa42dc65325700
evantype <evantype@gmail.com>
2022-08-28 22:15:51 +0300

    сделать райтбар

---
+++ resources/views/partitials/rightbar.blade.php
@@ -0,0 +1,16 @@
+<div class="col-md-4">
+    <div class="position-sticky" style="top: 2rem;">
+        <div class="p-4 mb-3 bg-light rounded">
+            <h4>О нас</h4>
+            <p class="mb-0">Мы - группа разработчиков и специалистов объединенных общими интересами</p>
+        </div>
+
+        <div class="p-4">
+            <h4>Контакты:</h4>
+            <ol class="list-unstyled">
+                <li><a class="link" href="https://github.com/evantype" target="_blank">GitHub</a></li>
+                <li><a href="https://t.me/evantype" target="_blank">Telegram</a></li>
+            </ol>
+        </div>
+    </div>
+</div>
