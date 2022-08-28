4bdb96feef01ec2dcd4488bcfc4de4624a32dc9d
evantype <evantype@gmail.com>
2022-08-29 00:37:48 +0300

    поставить vue3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,9 @@
 import './bootstrap';
 import {createApp} from "vue";
-import SiteTemplatePreviewComponent from "./components/site-template-preview-component.vue";
+import { Fancybox } from "@fancyapps/ui";
 
 const app = createApp({});
 
-app.component('site-template-preview', SiteTemplatePreviewComponent)
+// app.component('site-template-preview', SiteTemplatePreviewComponent)
 
 app.mount('#app');
